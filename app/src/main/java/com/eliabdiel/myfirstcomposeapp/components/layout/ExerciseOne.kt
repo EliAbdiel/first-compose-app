3ee3cc761912ffdc87bb9449dc93265d98160d4e
EliAbdiel <371.abdiel@gmail.com>
2025-02-08 17:53:40 -0500

    add Spacer to Layouts

--- app/src/main/java/com/eliabdiel/myfirstcomposeapp/components/layout/ExerciseOne.kt
+++ app/src/main/java/com/eliabdiel/myfirstcomposeapp/components/layout/ExerciseOne.kt
@@ -1,34 +1,40 @@
 package com.eliabdiel.myfirstcomposeapp.components.layout
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun ExerciseOne(modifier: Modifier=Modifier) {
     Column(modifier = modifier) {
-        Box(modifier = Modifier.weight(1f).fillMaxSize().background(Color.Cyan), contentAlignment = Alignment.Center) {
+        Box(modifier = Modifier
+                // .padding(bottom = 20.dp) // sustituimos el padding por un Spacer
+                .weight(1f)
+                .fillMaxSize()
+                .background(Color.Cyan),
+            contentAlignment = Alignment.Center) {
             Text("Ejemplo 1")
         }
+        Spacer(Modifier.height(20.dp))
         Box(modifier = Modifier.weight(1f).fillMaxSize().background(Color.White)) {
             Row{
                 Box(modifier = Modifier.weight(1f).fillMaxSize().background(Color.Red), contentAlignment = Alignment.Center) {
                     Text("Ejemplo 2")
                 }
+                Spacer(Modifier.width(20.dp))
                 Box(modifier = Modifier.weight(1f).fillMaxSize().background(Color.Green), contentAlignment = Alignment.Center) {
                     Text("Ejemplo 3")
                 }
             }
         }
+        Spacer(Modifier.height(20.dp))
         Box(modifier = Modifier.weight(1f).fillMaxSize().background(Color.Magenta), contentAlignment = Alignment.BottomCenter) {
             Text("Ejemplo 4")
         }
