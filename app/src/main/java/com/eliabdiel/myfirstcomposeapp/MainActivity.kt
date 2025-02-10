8b7da0be07471e838e5659c9e55d721b895cdea4
EliAbdiel <371.abdiel@gmail.com>
2025-02-10 10:46:05 -0500

    add basic ConstrainLayout to Layouts

--- app/src/main/java/com/eliabdiel/myfirstcomposeapp/MainActivity.kt
+++ app/src/main/java/com/eliabdiel/myfirstcomposeapp/MainActivity.kt
@@ -10,9 +10,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.eliabdiel.myfirstcomposeapp.components.layout.ExerciseOne
-//import com.eliabdiel.myfirstcomposeapp.components.layout.MyBox
-//import com.eliabdiel.myfirstcomposeapp.components.layout.MyColumn
+// import com.eliabdiel.myfirstcomposeapp.components.layout.ExerciseOne
+import com.eliabdiel.myfirstcomposeapp.components.layout.MyBasicConstrainLayout
+// import com.eliabdiel.myfirstcomposeapp.components.layout.MyBox
+// import com.eliabdiel.myfirstcomposeapp.components.layout.MyColumn
 import com.eliabdiel.myfirstcomposeapp.login.Greeting
 import com.eliabdiel.myfirstcomposeapp.ui.theme.MyFirstComposeAppTheme
 
@@ -32,7 +33,8 @@ class MainActivity : ComponentActivity() {
                     // MyColumn(modifier = Modifier.padding(innerPadding))
                     // MyRow(modifier = Modifier.padding(innerPadding))
                     // MyComplexLayout(modifier = Modifier.padding(innerPadding))
-                    ExerciseOne(modifier = Modifier.padding(innerPadding))
+                    // ExerciseOne(modifier = Modifier.padding(innerPadding))
+                    MyBasicConstrainLayout(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
